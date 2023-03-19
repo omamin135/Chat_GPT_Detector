d7f1a7155c37c006c340254496206b77c9785b68
Om Amin <omamin135@gmail.com>
2023-03-18 20:00:29 -0400

    splits each sentence into array of words

--- process_data.py
+++ process_data.py
@@ -13,28 +13,42 @@ excelFile = "Data.xlsx"
 
 #clean inputed line string by removeing any punctuation or unneeded characters
 def cleanLine(line):
-    clean = line.replace(',', '')
+
+    #remove special characters and punctuation from the line
+    clean = line.translate(str.maketrans('', '', ",'\"?<>*!@$"))
 
     return clean
+
+#generates a character array for the word
+def splitWord(word):
+    return [char for char in word]
 
 with open(inputFile, 'r') as inFile:
 
     textArray = []
     for line in inFile:
-        
+
         #remove punctuation and extraneous chars from line
         clean = cleanLine(line)
 
         #spead the line split at every '.' into the array
-        senArr = [sentence.strip() for sentence in clean.split(".")]
-        
+        senArr = [sentence.strip() for sentence in clean.split('.')]
+
         #move all non-blank entires from sentence array into main data array
         for sen in senArr:
             if (sen != ''):
                 textArray.append(sen)
 
+#creates and 2D array of the split words in each sentence
+wordArr = []
+for i, sentence in enumerate(textArray):
+    wordArr.append([word.strip() for word in sentence.split(' ')])
+
+
+
+
 #export the sentence data to excel spreadsheet
-marks_data = pd.DataFrame({"sentences":textArray})
+marks_data = pd.DataFrame({"sentences":textArray, "word": wordArr})
 marks_data.to_excel(excelFile)
 
-os.startfile(excelFile) 
+# os.startfile(excelFile) 
